6dfac4b465f68d5d73774f963f273e2957837def
Eddie Kim <ksh940911@naver.com>
2021-01-24 05:02:20 +0900

    2021.01.24 PRACTICE

--- oracle_practice.sql
+++ oracle_practice.sql
@@ -16,10 +16,8 @@
 	select * from tbl_escape_watch;
 --정답
     select description
-    from tbl_escape_watch;
-    
-    select description
-    from tbl_escape_watch;
+    from tbl_escape_watch
+    where description like '%99.99\%%' escape '\';
     
 --@실습문제 2 : 파일경로를 제외하고 파일명만 아래와 같이 출력하세요.
 /*
@@ -31,7 +29,7 @@
 2             music.mp3
 3             resume.hwp
 ---------------------------
-*/
+*/ 
 	create table tbl_files
 	(fileno number(3)
 	,filepath varchar2(500)
@@ -45,6 +43,12 @@
 
 	select * 
 	from tbl_files;
+--정답
+    select rpad(fileno,1) 파일번호, substr(filepath, (instr(filepath,'\',-1)+1)) 파일명
+    from tbl_files;
+    
+    select instr(filepath,'\',-1) --substr돌릴때 filepath에서 자르기 시작할 인덱스 넘버 찾기. 근데 찾고나서 \를 출력하고 싶지않으니 +1을 해주면 \를 빼고 출력할수있음
+    from tbl_files;
     
     
     
