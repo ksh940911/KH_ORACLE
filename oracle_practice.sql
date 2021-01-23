c786beb74c610ff82f510eef25fe1d0174832493
Eddie Kim <ksh940911@naver.com>
2021-01-24 03:30:19 +0900

    2021.01.24 PRACTICE

--- oracle_practice.sql
+++ oracle_practice.sql
@@ -1,2 +1,25 @@
---for homework
---n1문제
+--==============================
+-- FOR PRACTICE
+--==============================
+--@실습문제 1 : tbl_escape_watch 테이블에서 description 컬럼에 99.99% 라는 글자가 들어있는 행만 추출하세요.
+	create table tbl_escape_watch(
+		watchname   varchar2(40)
+		,description    varchar2(200)
+	);
+	--drop table tbl_escape_watch;
+	insert into tbl_escape_watch values('금시계', '순금 99.99% 함유 고급시계');
+	insert into tbl_escape_watch values('은시계', '고객 만족도 99.99점를 획득한 고급시계');
+	commit;
+	select * from tbl_escape_watch;
+    
+--@실습문제 2 : 파일경로를 제외하고 파일명만 아래와 같이 출력하세요.
+/*
+출력결과 :
+---------------------------
+파일번호          파일명
+---------------------------
+1             salesinfo.xls
+2             music.mp3
+3             resume.hwp
+---------------------------
+*/
